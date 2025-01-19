3c1adc9f3c7e8f66dc9981c809a6eb57da47e28b
Roderick Fitzgerald <liber6schol@outlook.com>
2025-01-19 01:39:37 -0500

    updated tasks in readme and finished conditional in assignment.js, still need to get eventListener to function in html.

--- js/assignment.js
+++ js/assignment.js
@@ -2,16 +2,23 @@
 
 // Create your references to the html elements here
 const ageInputEl = document.getElementById("age-input")
+const checkAgeAndRespond = document.getElementById("response")
 const submissionBtn = document.getElementById("submission-btn");
 
 // create a mutable variable called age and do not assign it a value.
-
+let age;
 
 function checkAgeAndRespond() {
     age = parseInt(ageInputEl.value);
-    
-    // Write your code below
 
+    // Write your code below
+  if (age >= 21) {
+        console.log("You can vote and purchase alcohol.");
+   } else if (age >= 18) {
+    console.log("You can vote, but you cannot purchase alcohol.");
+   } else if (age < 18) {
+        console.log("You cannot vote and you cannot purchase alcohol.");
+    }
 }
 
 
