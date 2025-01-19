13bae176b8d30a57edcf2de8744c93957f5ecfb9
Roderick Fitzgerald <liber6schol@outlook.com>
2025-01-19 00:18:52 -0500

    experimented and commented out everything in index.js and updated tasks in readme.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,19 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+
+/*
+let healthPoints = 100;
+
+if (healthPoints == 100) {
+    console.log("Hero is ready for anything!");
+} else if (healthPoints >= 50) {
+    console.log("Hero has light wounds.");
+} else if (healthPoints < 50 && healthPoints >= 20) {
+    console.log("Hero is heavily damaged.");
+} else if (healthPoints < 20 && healthPoints >= 5 ) {
+    console.log("Hero is critically wounded.");
+} else if (healthPoints < 10 && healthPoints >= 1) {
+    console.log("Hero is near deaths door!");
+}
+    */
