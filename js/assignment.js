30d67d3792c1401060234cc6e174f561211a36f5
Roderick Fitzgerald <liber6schol@outlook.com>
2025-01-19 17:38:03 -0500

    made adjustments to code; works in dev tools but still does not show in answer 'ouput'.

--- js/assignment.js
+++ js/assignment.js
@@ -1,24 +1,25 @@
 "use strict";
 
 // Create your references to the html elements here
-const ageInputEl = document.getElementById("age-input")
-const checkAgeAndRespond = document.getElementById("response")
+const ageInputEl = document.getElementById("age-input");
+const answerOuput = document.getElementById("response");
 const submissionBtn = document.getElementById("submission-btn");
 
 // create a mutable variable called age and do not assign it a value.
 let age;
 
 function checkAgeAndRespond() {
-    age = parseInt(ageInputEl.value);
+  age = parseInt(ageInputEl.value);
 
-    // Write your code below
+  // Write your code below
   if (age >= 21) {
-        console.log("You can vote and purchase alcohol.");
-   } else if (age >= 18) {
+    console.log("You can vote and purchase alcohol.");
+  } else if (age >= 18 && age < 21) {
     console.log("You can vote, but you cannot purchase alcohol.");
-   } else if (age < 18) {
-        console.log("You cannot vote and you cannot purchase alcohol.");
-    }
+  } else {
+    age < 18;
+    console.log("You cannot vote and you cannot purchase alcohol.");
+  }
 }
 
 
@@ -28,10 +29,10 @@ submissionBtn.addEventListener("click", function () {
 
 
 function validateInput(event) {
-    const invalidCharacters = ["e", "E", "-", "0", "+"];
-    if (invalidCharacters.includes(event.key)) {
-        event.preventDefault();
-    }
+  const invalidCharacters = ["e", "E", "-", "0", "+"];
+  if (invalidCharacters.includes(event.key)) {
+    event.preventDefault();
+  }
 }
 
 ageInputEl.addEventListener("keydown", validateInput);
